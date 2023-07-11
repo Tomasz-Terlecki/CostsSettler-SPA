c3f120f735b8640ffd7633d7607cd3720ed00295
Tomasz Terlecki <t.terlecki@wp.pl>
2023-07-11 23:28:27 +0200

    Create environment config file

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  name: 'dev',
+  keycloakConfig: {
+    url: 'http://costssettler:8080/',
+    realm: 'CostsSettler',
+    clientId: 'CostsSettler-Frontend'
+  },
+  baseUrl: 'http://costssettler.com:8082/api'
+}
