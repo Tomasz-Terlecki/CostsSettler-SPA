8d2a62bc512bab4260baf550624ef8ba5dc47e07
Tomasz Terlecki <t.terlecki@wp.pl>
2023-07-12 15:32:02 +0200

    Add keycloak login to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,50 @@
-import { NgModule } from '@angular/core';
+import { AuthGuard } from './guards/auth-guard';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+import { environment } from 'src/environments/environment';
+import { ChargeComponent } from './components/charges/charge/charge.component';
+import { ChargeListComponent } from './components/charges/charge-list/charge-list.component';
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: environment.keycloakConfig.url,
+        realm: environment.keycloakConfig.realm,
+        clientId: environment.keycloakConfig.clientId,
+      },
+      initOptions: {
+        onLoad: 'check-sso',
+        redirectUri: window.location.href,
+        checkLoginIframe: false
+      }
+    });
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChargeComponent,
+    ChargeListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    KeycloakAngularModule
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
