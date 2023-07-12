28438e248fb3f253ae8fab8e8e37b5e8f12c5a08
Tomasz Terlecki <t.terlecki@wp.pl>
2023-07-12 15:32:12 +0200

    Add AuthGuard to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth-guard';
+import { ChargeListComponent } from './components/charges/charge-list/charge-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    canActivate: [AuthGuard],
+    component: ChargeListComponent,
+    children: []
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
