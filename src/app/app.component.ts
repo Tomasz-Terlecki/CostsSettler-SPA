9db2256cc6353c1e5a9181490644025b66d1cfff
Tomasz Terlecki <t.terlecki@wp.pl>
2023-07-17 16:28:24 +0200

    Setting/removing current user on init/destroy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'CostsSettler';
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.setCurrentUser();
+  }
+
+  ngOnDestroy(): void {
+    this.authService.removeCurrentUser();
+  }
+  
 }
