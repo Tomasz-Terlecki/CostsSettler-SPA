880aa56048eca657133780273548fc37b5dfe57e
Tomasz Terlecki <t.terlecki@wp.pl>
2023-07-18 16:39:36 +0200

    Move get method out of base service

--- src/app/services/base.service.ts
+++ src/app/services/base.service.ts
@@ -1,23 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { BaseModel } from '../models/base.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export abstract class BaseService<TModel extends BaseModel> {
+export abstract class BaseService {
 
   baseUrl: string;
 
   constructor(protected httpClient: HttpClient, controllerName: string) {
     this.baseUrl = 'http://costssettler.com:8082/api/' + controllerName;
-  }
-
-  get(queryParams: any): Observable<Array<TModel>> {
-    return this.httpClient.get<Array<TModel>>(this.baseUrl, {
-      params: queryParams
-    });
   }
 
 }
--- src/app/services/charges.service.ts
+++ src/app/services/charges.service.ts
@@ -1,15 +1,22 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
 import { BaseService } from './base.service';
 import { HttpClient } from '@angular/common/http';
-import { Charge } from '../models/charge.model';
+import { ChargeForListDto } from '../models/dtos/charge-for-list.dto';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ChargesService extends BaseService<Charge> {
+export class ChargesService extends BaseService {
 
   constructor(httpClient: HttpClient) {
     super(httpClient, 'charges');
+  }
+
+  get(queryParams: any): Observable<Array<ChargeForListDto>> {
+    return this.httpClient.get<Array<ChargeForListDto>>(this.baseUrl, {
+      params: queryParams
+    });
   }
 
 }
