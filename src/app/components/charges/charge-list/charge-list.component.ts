388042beccd8a161b62b83bac38f7f70aa435c65
Tomasz Terlecki <t.terlecki@wp.pl>
2023-07-18 17:49:34 +0200

    Charge list component

--- src/app/components/charges/charge-list/charge-list.component.ts
+++ src/app/components/charges/charge-list/charge-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ChargeForListDto } from 'src/app/models/dtos/charge-for-list.dto';
+import { AuthService } from 'src/app/services/auth.service';
+import { ChargesService } from 'src/app/services/charges.service';
 
 @Component({
   selector: 'app-charge-list',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./charge-list.component.scss']
 })
 export class ChargeListComponent implements OnInit {
+  charges: Array<ChargeForListDto> | undefined;
 
-  constructor() { }
+  constructor(private chargesService: ChargesService, private authService: AuthService) { }
 
   ngOnInit() {
+    this.chargesService.get({
+        userId: this.authService.currentUser?.id
+      })
+      .subscribe({
+        next: (res) => { this.charges = res },
+        error: (err) => { console.error(err) }
+      });
   }
 
 }
