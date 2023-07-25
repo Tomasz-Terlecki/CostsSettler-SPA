51fe7da0f754b29e25683422e6425e65645d4abe
Tomasz Terlecki <t.terlecki@wp.pl>
2023-07-25 08:24:38 +0200

    Charge component buttons logic implementation

--- src/app/components/charges/charge/charge.component.ts
+++ src/app/components/charges/charge/charge.component.ts
@@ -1,7 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { ChargeStatus } from 'src/app/enums/charge-status.enum';
+import { ChargeVote } from 'src/app/enums/charge-vote.enum';
 import { CircumstanceStatus } from 'src/app/enums/circumstance.status';
 import { ChargeForListDto } from 'src/app/models/dtos/charge-for-list.dto';
 import { AuthService } from 'src/app/services/auth.service';
+import { ChargesService } from 'src/app/services/charges.service';
 
 @Component({
   selector: 'app-charge',
@@ -9,28 +12,61 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./charge.component.scss']
 })
 export class ChargeComponent {
-  @Input() charge: ChargeForListDto | undefined;
+  @Input() charge!: ChargeForListDto;
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService, 
+    private chargesService: ChargesService) {}
   
   get isVoteEnabled(): boolean {
-    return this.charge?.debtorId === this.authService.currentUser?.id;
+    if (this.charge.chargeStatus === ChargeStatus.Settled ||
+        this.charge.circumstanceStatus === CircumstanceStatus.PartiallySettled ||
+        this.charge.circumstanceStatus === CircumstanceStatus.Settled) {
+      return false;
+    }
+    
+    return this.charge.debtorId === this.authService.currentUser?.id;
   }
 
   get isSettleEnabled(): boolean {
-    return this.charge?.circumstanceStatus === CircumstanceStatus.Accepted
-      && this.charge?.creditorId === this.authService.currentUser?.id;
-  }
-
-  settleCharge(): void {
-
+    if (this.charge.chargeStatus !== ChargeStatus.Accepted ||
+        this.charge.circumstanceStatus !== CircumstanceStatus.Accepted && 
+        this.charge.circumstanceStatus !== CircumstanceStatus.PartiallySettled) {
+      return false;
+    }
+    return this.charge.creditorId === this.authService.currentUser?.id;
   }
 
   acceptCharge(): void {
-    
+    this.chargesService.voteForCharge(this.charge.id, ChargeVote.Accept)
+      .subscribe({
+        next: (res) => {
+          // Get By Id? Or get full charge list?
+          // this.chargesService.
+        },
+        error: (err) => { console.error(err); }
+      });
   }
   
   rejectCharge(): void {
-    
+    this.chargesService.voteForCharge(this.charge.id, ChargeVote.Reject)
+      .subscribe({
+        next: (res) => {
+          // Get By Id? Or get full charge list?
+          // this.chargesService.
+        },
+        error: (err) => { console.error(err); }
+      });
+  }
+
+  settleCharge(): void {
+    this.chargesService.settleCharge(this.charge.id)
+      .subscribe({
+        next: (res) => {
+          // Get By Id? Or get full charge list?
+          // this.chargesService.
+        },
+        error: (err) => { console.error(err); }
+      });
   }
 }
