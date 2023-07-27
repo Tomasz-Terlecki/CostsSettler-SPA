027e84c4f9d8a63788a5627c7a5965891d787c14
Tomasz Terlecki <t.terlecki@wp.pl>
2023-07-27 17:08:43 +0200

    Circumstance component implementation

--- src/app/components/circumstances/circumstance/circumstance.component.ts
+++ src/app/components/circumstances/circumstance/circumstance.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CircumstanceStatus } from 'src/app/enums/circumstance.status';
+import { CircumstanceForListDto } from 'src/app/models/dtos/circumstance-for-list.dto';
 
 @Component({
   selector: 'app-circumstance',
   templateUrl: './circumstance.component.html',
   styleUrls: ['./circumstance.component.scss']
 })
-export class CircumstanceComponent implements OnInit {
+export class CircumstanceComponent {
+  @Input() circumstance!: CircumstanceForListDto;
 
   constructor() { }
 
-  ngOnInit() {
+  viewCircumstance(): void {
+    // TODO
   }
 
+  setCardClass(): any {
+    return {
+      'border-color-rejected': this.circumstance.circumstanceStatus === CircumstanceStatus.Rejected,
+      'border-color-settled': this.circumstance.circumstanceStatus === CircumstanceStatus.Settled
+    };
+  }
+
+  setCardHeaderClass(): any {
+    return {
+      'background-rejected': this.circumstance.circumstanceStatus === CircumstanceStatus.Rejected,
+      'background-settled': this.circumstance.circumstanceStatus === CircumstanceStatus.Settled
+    };
+  }
 }
--- src/app/models/dtos/circumstance-for-list.dto.ts
+++ src/app/models/dtos/circumstance-for-list.dto.ts
@@ -1,20 +1,16 @@
 import { CircumstanceStatus } from "src/app/enums/circumstance.status";
-import { User } from "../user.model";
 
 export class CircumstanceForListDto {
   id: string;
   description: string;
   totalAmount: number;
   circumstanceStatus: CircumstanceStatus;
-  creditor: User | undefined;
-  creditorId: string;
   dateTime: Date | undefined;
 
   constructor() {
     this.id = '';
     this.description = '';
     this.totalAmount = 0;
-    this.creditorId = '';
     this.circumstanceStatus = CircumstanceStatus.None;
   }
 }
