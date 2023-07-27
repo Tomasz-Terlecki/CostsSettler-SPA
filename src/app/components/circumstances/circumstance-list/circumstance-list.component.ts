778ec0238ac4de72b2a2231dd5c143e9fba82664
Tomasz Terlecki <t.terlecki@wp.pl>
2023-07-27 16:27:45 +0200

    Circumstance list implementation

--- src/app/components/circumstances/circumstance-list/circumstance-list.component.ts
+++ src/app/components/circumstances/circumstance-list/circumstance-list.component.ts
@@ -1,4 +1,8 @@
+import { CircumstancesService } from 'src/app/services/circumstances.service';
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { CircumstanceForListDto } from 'src/app/models/dtos/circumstance-for-list.dto';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-circumstance-list',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./circumstance-list.component.scss']
 })
 export class CircumstanceListComponent implements OnInit {
+  circumstances: Array<CircumstanceForListDto> | undefined;
 
-  constructor() { }
+  constructor(private circumstancesService: CircumstancesService, 
+    private authService: AuthService, private toastrService: ToastrService) { }
 
   ngOnInit() {
+    this.getData();
+  }
+
+  getData(): void {
+    this.circumstancesService.get({
+      userId: this.authService.currentUser?.id
+    })
+      .subscribe({
+        next: (res) => { this.circumstances = res; },
+        error: (err) => {
+          console.error(err); 
+          this.toastrService.error('Downloading circumstances failed')
+        }
+      })
   }
 
 }
