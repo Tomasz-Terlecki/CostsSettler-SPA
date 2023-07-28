bfa28d41e75916586768c5e06e2d904ee51d49a5
Tomasz Terlecki <t.terlecki@wp.pl>
2023-07-28 07:59:24 +0200

    Getting circumstance data

--- src/app/components/circumstances/detailed-circumstance/detailed-circumstance.component.ts
+++ src/app/components/circumstances/detailed-circumstance/detailed-circumstance.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Circumstance } from 'src/app/models/circumstance.model';
+import { CircumstancesService } from 'src/app/services/circumstances.service';
 
 @Component({
   selector: 'app-detailed-circumstance',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detailed-circumstance.component.scss']
 })
 export class DetailedCircumstanceComponent implements OnInit {
+  circumstance!: Circumstance;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private circumstancesService: CircumstancesService,
+    private toastrService: ToastrService) { }
 
   ngOnInit() {
+    this.getCircumstance();
+  }
+  
+  getCircumstance() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.circumstancesService.getById(id!)
+      .subscribe({
+        next: (res) => { this.circumstance = res; },
+        error: (err) => {
+          console.error(err); 
+          this.toastrService.error('Downloading circumstances failed')
+        }
+      })
+    
   }
 
 }
