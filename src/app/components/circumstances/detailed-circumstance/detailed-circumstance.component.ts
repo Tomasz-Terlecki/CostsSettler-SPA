4078d5c005a5a64bc9b5cb94ce32f3a5cfab7846
Tomasz Terlecki <t.terlecki@wp.pl>
2023-07-28 15:13:00 +0200

    Base circumstance detailed

--- src/app/components/circumstances/detailed-circumstance/detailed-circumstance.component.ts
+++ src/app/components/circumstances/detailed-circumstance/detailed-circumstance.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Circumstance } from 'src/app/models/circumstance.model';
@@ -11,25 +12,44 @@ import { CircumstancesService } from 'src/app/services/circumstances.service';
 })
 export class DetailedCircumstanceComponent implements OnInit {
   circumstance!: Circumstance;
+  circumstanceForm!: FormGroup;
+  // users: UserForListDto[];
 
-  constructor(private route: ActivatedRoute, private circumstancesService: CircumstancesService,
-    private toastrService: ToastrService) { }
+  constructor(private route: ActivatedRoute, 
+    private circumstancesService: CircumstancesService, private toastrService: ToastrService
+  ) {
+    this.setForm();
+  }
 
   ngOnInit() {
     this.getCircumstance();
   }
   
-  getCircumstance() {
+  getCircumstance(): void {
     const id = this.route.snapshot.paramMap.get('id');
     this.circumstancesService.getById(id!)
       .subscribe({
-        next: (res) => { this.circumstance = res; },
+        next: (res) => { 
+          this.circumstance = res;
+        },
         error: (err) => {
           console.error(err); 
           this.toastrService.error('Downloading circumstances failed')
         }
       })
-    
+  }
+
+  setForm(): void {
+    this.circumstanceForm = new FormGroup({
+      description: new FormControl<string>('', Validators.required),
+      totalAmount: new FormControl<number>(0, Validators.required),
+      date: new FormControl<Date | null>(null, Validators.required),
+      time: new FormControl<Date | null>(null, Validators.required),
+    })
+  }
+
+  save(): void {
+    console.log(this.circumstanceForm.value);
   }
 
 }
