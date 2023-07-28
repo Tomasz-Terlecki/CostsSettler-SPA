42a9eaa319c6800e001f0ac2dfae2faf3ee3efef
Tomasz Terlecki <t.terlecki@wp.pl>
2023-07-28 15:23:20 +0200

    Getting users for dropdown

--- src/app/components/circumstances/detailed-circumstance/detailed-circumstance.component.ts
+++ src/app/components/circumstances/detailed-circumstance/detailed-circumstance.component.ts
@@ -3,7 +3,9 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Circumstance } from 'src/app/models/circumstance.model';
+import { UserForListDto } from 'src/app/models/dtos/user-for-list.dto';
 import { CircumstancesService } from 'src/app/services/circumstances.service';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-detailed-circumstance',
@@ -13,16 +15,17 @@ import { CircumstancesService } from 'src/app/services/circumstances.service';
 export class DetailedCircumstanceComponent implements OnInit {
   circumstance!: Circumstance;
   circumstanceForm!: FormGroup;
-  // users: UserForListDto[];
+  users!: UserForListDto[];
 
-  constructor(private route: ActivatedRoute, 
-    private circumstancesService: CircumstancesService, private toastrService: ToastrService
+  constructor(private route: ActivatedRoute, private circumstancesService: CircumstancesService,
+    private toastrService: ToastrService, private usersService: UsersService
   ) {
     this.setForm();
   }
 
   ngOnInit() {
     this.getCircumstance();
+    this.getUsers();
   }
   
   getCircumstance(): void {
@@ -34,9 +37,22 @@ export class DetailedCircumstanceComponent implements OnInit {
         },
         error: (err) => {
           console.error(err); 
-          this.toastrService.error('Downloading circumstances failed')
+          this.toastrService.error('Downloading circumstances failed');
         }
       })
+  }
+
+  getUsers(): void {
+    this.usersService.get({})
+      .subscribe({
+        next: (res) => {
+          this.users = res;
+        },
+        error: (err) => {
+          console.error(err);
+          this.toastrService.error('Downloading users failed');
+        }
+      });
   }
 
   setForm(): void {
