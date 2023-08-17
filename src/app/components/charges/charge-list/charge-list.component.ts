cfde2c979b15c5c0a2eff0974406bde156e3a027
Tomasz Terlecki <t.terlecki@wp.pl>
2023-08-17 22:59:41 +0200

    AQdd date from and date to filtering in charge list

--- src/app/components/charges/charge-list/charge-list.component.ts
+++ src/app/components/charges/charge-list/charge-list.component.ts
@@ -1,5 +1,8 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
+import { ChargeStatus } from 'src/app/enums/charge-status.enum';
 import { ChargeForListDto } from 'src/app/models/dtos/charge-for-list.dto';
 import { AuthService } from 'src/app/services/auth.service';
 import { ChargesService } from 'src/app/services/charges.service';
@@ -11,17 +14,24 @@ import { ChargesService } from 'src/app/services/charges.service';
 })
 export class ChargeListComponent implements OnInit {
   charges: Array<ChargeForListDto> | undefined;
+  filterForm!: FormGroup;
 
   constructor(private chargesService: ChargesService, 
-    private authService: AuthService, private toastrService: ToastrService) { }
+      private authService: AuthService, private toastrService: ToastrService
+  ) {
+    this.setForm();
+  }
 
   ngOnInit() {
     this.getData();
   }
 
   getData(): void {
+    let params = this.filterForm.value;
     this.chargesService.get({
-      userId: this.authService.currentUser?.id
+      userId: this.authService.currentUser?.id,
+      dateFrom: params['dateFrom'],
+      dateTo: params['dateTo']
     })
       .subscribe({
         next: (res) => { this.charges = res; },
@@ -30,6 +40,20 @@ export class ChargeListComponent implements OnInit {
           this.toastrService.error('Downloading charges failed')
         }
       });
+  }
+
+  setForm(): void {
+    let dateFrom = new Date();
+    dateFrom.setDate(dateFrom.getDate() - 10);
+
+    const dateTo = new Date();
+
+    this.filterForm = new FormGroup({
+      chargeStatus: new FormControl<ChargeStatus | null>(null, Validators.required),
+      dateFrom: new FormControl<string>(formatDate(dateFrom, 'yyyy-MM-dd', 'en'), Validators.required),
+      dateTo: new FormControl<string>(formatDate(dateTo, 'yyyy-MM-dd', 'en'), Validators.required),
+      circumstanceDescription: new FormControl<string | null>(null, Validators.required),
+    })
   }
 
 }
