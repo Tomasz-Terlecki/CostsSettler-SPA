af84056fcd7d3449621d6c8c1eedfdc5c138793d
Tomasz Terlecki <t.terlecki@wp.pl>
2023-08-28 17:32:08 +0200

    Charge list tests

---
+++ cypress/e2e/charges/charge-list.cy.ts
@@ -0,0 +1,38 @@
+/// <reference types="cypress" />
+import { login, openPage } from '../../support/methods'
+
+describe('Charge list tests', () => {
+
+  before(() => {
+    login();
+  })
+
+  beforeEach(() => {
+    openPage('http://costssettler.com/charges');
+  });
+
+  it('Navbar test', () => {
+    cy.get('.navbar').should('be.visible');
+  });
+
+  it('Filters form visibility test', () => {
+    cy.get('.filters-form').should('be.visible');
+    cy.get('#dateFromInput').should('be.visible');
+    cy.get('#dateToInput').should('be.visible');
+    cy.get('#searchInput').should('be.visible');
+    cy.get('#amountFrom').should('be.visible');
+    cy.get('#amountTo').should('be.visible');
+  });
+
+  it('Filter button visibility test', () => {
+    cy.get('.filter-button')
+      .should('be.visible')
+      .should('contain', 'Filter');
+  });
+
+  it('Filter button visibility test', () => {
+    cy.get('app-charge-list')
+      .should('be.visible');
+  });
+
+});
