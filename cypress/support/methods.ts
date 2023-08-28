31efaf031a97c7e2b2ffd5e50e71b19976d41a64
Tomasz Terlecki <t.terlecki@wp.pl>
2023-08-28 17:32:36 +0200

    Tests methods add

--- cypress/support/methods.ts
+++ cypress/support/methods.ts
@@ -17,3 +17,33 @@ export const setMobileViewport = () => {
   // iPhone 13 viewport
   cy.viewport(390, 844);
 }
+
+export const currentYear = (): string => new Date().getFullYear().toString();
+
+export const currentMonth = (): string => {
+  let month = (new Date().getMonth() + 1).toString();
+  month = month.length === 1 
+    ? '0' + month
+    : month;
+  return month;
+};
+
+export const currentDay = (): string => {
+  let day = new Date().getDate().toString();
+  day = day.length === 1 
+    ? '0' + day
+    : day;
+  return day;
+}
+
+export const addExampleCircumstance = () => {
+  openPage('http://costssettler.com/circumstance/00000000-0000-0000-0000-000000000000');
+  cy.get('#amountInput').type('100');
+  cy.get('#descriptionInput').type('Test description')
+  cy.get('#dateInput').type(`${currentYear()}-${currentMonth()}-${currentDay()}`);
+  cy.get('#timeInput').type('10:10');
+  cy.get('#debtorsSelect')
+    .select('Test1 Test1');
+
+  cy.get('.save-button').click();
+}
