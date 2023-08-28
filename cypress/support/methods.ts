dc6749f894ef64ab116ffe26192728c2ac125f34
Tomasz Terlecki <t.terlecki@wp.pl>
2023-08-28 14:30:17 +0200

    basic helper methods

---
+++ cypress/support/methods.ts
@@ -0,0 +1,14 @@
+export const openPage = (pageUrl: any) => {
+  cy.intercept('GET', pageUrl).as('getData');
+  cy.visit(pageUrl);
+  cy.wait('@getData');
+};
+
+export const login = () => {
+  cy.clearLocalStorage();
+  cy.clearAllCookies();
+  openPage('http://costssettler.com/');
+  cy.get('#username').type('test@gmail.com');
+  cy.get('#password').type('password');
+  cy.get('#kc-login').click()
+};
