e4ff29f54dfc77b8746907092fd272254098f737
Tomasz Terlecki <t.terlecki@wp.pl>
2023-08-28 17:27:46 +0200

    Existing circumstance form tests

---
+++ cypress/e2e/circumstances/existing-circumstance-form.cy.ts
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+import { addExampleCircumstance, currentDay, currentMonth, currentYear, login, openPage } from '../../support/methods'
+
+describe('Existing circumstance form tests', () => {
+
+  before(() => {
+    login();
+    addExampleCircumstance();
+  })
+
+  beforeEach(() => {
+    openPage('http://costssettler.com/circumstances');
+    cy.get('.action-button')
+      .first()
+      .click();
+  });
+
+  it('Navbar test', () => {
+    cy.get('.navbar').should('be.visible');
+  });
+
+  it('Adding circumstance form test', () => {
+    cy.get('#amountInput')
+      .should('be.disabled');
+    cy.get('#descriptionInput')
+      .should('be.disabled');
+    cy.get('#dateInput')
+      .should('be.disabled');
+    cy.get('#timeInput')
+      .should('be.disabled');
+  });
+
+});
