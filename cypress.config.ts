a55cbf763877947819a80ee1761367c219773eff
Tomasz Terlecki <t.terlecki@wp.pl>
2023-08-28 14:28:25 +0200

    cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,17 +1,24 @@
-import { defineConfig } from "cypress";
+const { defineConfig } = require('cypress')
 
-export default defineConfig({
-  component: {
-    devServer: {
-      framework: "angular",
-      bundler: "webpack",
-    },
-    specPattern: "**/*.cy.ts",
+module.exports = defineConfig({
+  reporter: 'junit',
+  reporterOptions: {
+    mochaFile: 'cypress/results/testResult-[hash].xml',
+    overwrite: false
   },
-
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    setupNodeEvents(on: any, config: any) {
     },
-  },
+    baseUrl: "http://costssettler.com/",
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
+    chromeWebSecurity: false,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 20000,
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    testIsolation: false,
+    numTestsKeptInMemory: 0,
+    video: false,
+    screenshotOnRunFailure: true
+  }
 });
